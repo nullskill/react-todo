9c05b7df8d20860da23969a7025f25752bf91771
Guess Who <D:\The Bat!>
2016-10-31 13:19:55 +0300

    Complete handleAddTodo

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import TodoList from 'TodoList';
 import AddTodo from 'AddTodo';
 import TodoSearch from 'TodoSearch';
+import uuid from 'node-uuid';
 
 var TodoApp = React.createClass({
 	getInitialState() {
@@ -10,16 +11,16 @@ var TodoApp = React.createClass({
 			searchText: '',
 			todos: [
 				{
-					id: 1,
+					id: uuid(),
 					text: 'Walk the dog'
 				}, {
-					id: 2,
+					id: uuid(),
 					text: 'Clean the yard'
 				}, {
-					id: 3,
+					id: uuid(),
 					text: 'Buy a newspaper'
 				}, {
-					id: 4,
+					id: uuid(),
 					text: 'Save the world!'
 				}
 			]
@@ -27,7 +28,15 @@ var TodoApp = React.createClass({
 	},
 
 	handleAddTodo(text) {
-		alert('new todo: ' + text)
+		this.setState({
+			todos: [
+				...this.state.todos,
+				{
+					id: uuid(),
+					text: text
+				}
+			]
+		});
 	},
 
 	handleSearch(showCompleted, searchText) {
