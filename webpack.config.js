0325ce94dd8720ef4aa40205543a39bcaf097bc0
Guess Who <mail@mail.com>
2016-11-05 20:59:03 +0300

    Add showCompleted & searchText reducers

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,8 @@
 		],
 		alias: {
 			applicationStyles: 'app/styles/app.scss',
-			actions: 'app/actions/actions.jsx'
+			actions: 'app/actions/actions.jsx',
+			reducers: 'app/reducers/reducers.jsx'
 		},
 
 		extensions: ['', '.js', '.jsx']
