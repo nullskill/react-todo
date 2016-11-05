9a613b1069757a768fc9faf365962a657558e2d7
Guess Who <mail@mail.com>
2016-11-06 00:24:46 +0300

    Add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -4,6 +4,17 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 import TodoApp from 'TodoApp';
 
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() => {
+  console.log('New state', store.getState());
+})
+
+store.dispatch(actions.addTodo('Clean the room'));
+store.dispatch(actions.setSearchText('room'));
+store.dispatch(actions.toggleShowCompleted());
+
 // Load foundation
 $(document).foundation();
 
