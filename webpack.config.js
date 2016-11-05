19df791ffc157e5274d121106afbdc21a0c0667e
Guess Who <mail@mail.com>
2016-11-05 14:02:15 +0300

    Add actions & action tests

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,8 @@
 			'./app/api'
 		],
 		alias: {
-			applicationStyles: 'app/styles/app.scss'
+			applicationStyles: 'app/styles/app.scss',
+			actions: 'app/actions/actions.jsx'
 		},
 
 		extensions: ['', '.js', '.jsx']
