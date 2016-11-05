d72b3d16093fd08d8cf5c576b017e92e39faac93
Guess Who <mail@mail.com>
2016-11-05 23:52:38 +0300

    Add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -7,25 +7,59 @@ describe('Reducers', () => {
   describe('searchTextReducer', () => {
     it('should set searchText', () => {
       var action = {
-      	type: 'SET_SEARCH_TEXT',
-      	searchText: 'dog'
+        type: 'SET_SEARCH_TEXT',
+        searchText: 'dog'
       };
 
-	  var res = reducers.searchTextReducer(df(''), df(action));
+    var res = reducers.searchTextReducer(df(''), df(action));
 
-	  expect(res).toEqual(action.searchText);
+    expect(res).toEqual(action.searchText);
     });
 
     describe('showCompletedReducer', () => {
-		it('should toggle showCompleted', () => {
-	      var action = {
-	      	type: 'TOGGLE_SHOW_COMPLETED'
-	      };
+    it('should toggle showCompleted', () => {
+        var action = {
+          type: 'TOGGLE_SHOW_COMPLETED'
+        };
 
-		  var res = reducers.showCompletedReducer(df(false), df(action));
+      var res = reducers.showCompletedReducer(df(false), df(action));
 
-		  expect(res).toBe(true);
-	    });      
+      expect(res).toBe(true);
+      });      
+    });
+  });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Walk the dog'
+      };
+
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle todo', () => {
+      var id = 123;
+      var todos = [{
+          id,
+          text: 'Clean the room',
+          completed: true,
+          createdAt: 123,
+          completedAt: 125
+        }];
+      var action = {
+        type: 'TOGGLE_TODO',
+        id
+      };
+
+      var res = reducers.todosReducer(df(todos), df(action));
+
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toEqual(undefined);
     });
   });
 });
